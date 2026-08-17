928aa7f5ef2e8fdf4e466beeb13041d56efdaacc
bimoadis <bimoadi310@gmail.com>
2026-08-17 13:20:16 +0700

    Update CA in InstallBar

--- web/components/home/InstallBar.tsx
+++ web/components/home/InstallBar.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 export function InstallBar() {
   const [copied, setCopied] = useState(false);
-  const command = "https://github.com/bimoadis/Buggy";
+  const command = "H78G5BJyidnNmGtunBmmzi4bPbQafwYCC6M2fZ2Ypumpy";
 
   const handleCopy = () => {
     navigator.clipboard.writeText(command);
